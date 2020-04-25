a106b5e88d4b842e1c09db19baac46e8b4ae3e09
Brian Price <brianprice88@gmail.com>
2020-04-25 13:19:52 -0400

    "More efficient solution"

--- largest_product_of_three/largest product of three.js
+++ largest_product_of_three/largest product of three.js
@@ -19,3 +19,12 @@ var largestProductOfThree = function(array) {
   return largest
 
 }
+
+// more efficient solution with sorting:
+
+var largestProductOfThree = function(array) {
+  if (array.length < 3) {return null}
+  var sorted = array.sort((a, b) => a > b ? 1 : -1)
+  var maxIndex = sorted.length - 1;
+  return Math.max(sorted[maxIndex] * sorted[maxIndex - 1] * sorted[maxIndex - 2], sorted[0] * sorted[1] * sorted[maxIndex])
+}
