20008b5b0775b4efc63af574551771b59d67685f
Brian Price <brianprice88@gmail.com>
2020-04-27 02:45:10 -0400

    "Power set simpler solution"

--- power_set/power set.js
+++ power_set/power set.js
@@ -4,16 +4,15 @@
 //    2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
 //   
 
-var powerSet = function(str) {
-var set = {};
+var powerSet = function (str) {
+  var set = [];
 
-var recurse = function(current, remaining) {
-  set[current.split('').sort().join('')] = true;
-  if (remaining.length === 0) {return}
-  for (var i = 0; i < remaining.length; i++) {
-    recurse(current + remaining[i], remaining.slice(0, i) + remaining.slice(i + 1))
+  var recurse = function (current, index) {
+    set.push(current);
+    for (var i = index; i < str.length; i++) {
+      recurse(current + str[i], i + 1)
+    }
   }
-}
-recurse('', str)
-return Object.keys(set);
+  recurse('', 0)
+  return set
 };	
