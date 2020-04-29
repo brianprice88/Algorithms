8961574c0116d12ceae7adb54705c32064d6cf51
Brian Price <brianprice88@gmail.com>
2020-04-28 22:08:53 -0400

    "add selection sort"

---
+++ selectionSort.js
@@ -0,0 +1,25 @@
+// start with leftmost element and replace it with the smallest element (if that element is smaller) in the remaining unsorted portion 
+// keep doing this process until reaching the 2nd to last element (since last element will already be sorted after)
+
+var selectionSort = function(array) {
+
+for (var i = 0; i < array.length - 1; i++) {
+var val = array[i];
+var min = Infinity;
+var minIndex = 0;
+  for (var j = i + 1; j < array.length; j++) {
+      if (array[j] < min) {
+          min = array[j]
+          minIndex = j
+      }
+  }
+  
+  if (min < val) {
+  array[i] = min;
+  array[minIndex] = val
+  }
+
+}
+
+return array;
+}
