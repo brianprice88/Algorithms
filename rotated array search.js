8cf8b94501e3f5da8fb1a8b3b5e14660f1fd06f5
Brian Price <55764132+brianprice88@users.noreply.github.com>
2020-05-11 14:44:37 -0400

    Update rotated array search.js

--- rotated array search.js
+++ rotated array search.js
@@ -1,7 +1,6 @@
 // Given a sorted array that has been rotated some number of items right or
 //   left, i.e. [0, 1, 2, 3, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2, 3]
-//   how can you efficiently find an element? For simplicity, you can assume
-//   that there are no duplicate elements in the array.
+//   how can you efficiently find an element?
 
 var rotatedArraySearch = function(rotated, target) {
 	if (rotated.length === 0) {return null}
@@ -11,7 +10,11 @@ var rotatedArraySearch = function(rotated, target) {
 	while (left <= right) {
 	var midpoint = Math.floor((left + right) / 2)
 	if (rotated[midpoint] === target) {return midpoint}
-	if (rotated[left] <= rotated[midpoint]) { // this half is softed
+	if (rotated[midpoint] === rotated[left] && rotated[midpoint] === rotated[right]) { // edge case if there are duplicates e.g. [1,3,1,1,1]
+		left ++; // in this case we can just increment left and decrement right pointer, to ignore the duplicates at the ends
+		right --
+	}
+	else if (rotated[left] <= rotated[midpoint]) { // this half is sorted
 		if (rotated[left] <= target && target < rotated[midpoint]) { // if target is in range, binary search
 			right = midpoint - 1;
 		} else {
