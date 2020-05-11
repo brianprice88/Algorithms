561990045d72c5bedb1b63d45859aaccbf15976d
Brian Price <55764132+brianprice88@users.noreply.github.com>
2020-05-11 01:05:12 -0400

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -1,8 +1,8 @@
 // start with leftmost element and replace it with the smallest element (if that element is smaller) in the remaining unsorted portion 
 // keep doing this process until reaching the 2nd to last element (since last element will already be sorted after)
+// time complexity: O(n^2)
 
 var selectionSort = function(array) {
-
 for (var i = 0; i < array.length - 1; i++) {
 var val = array[i];
 var min = Infinity;
@@ -12,14 +12,11 @@ var minIndex = 0;
           min = array[j]
           minIndex = j
       }
-  }
-  
+  } 
   if (min < val) {
   array[i] = min;
   array[minIndex] = val
   }
-
 }
-
 return array;
 }
