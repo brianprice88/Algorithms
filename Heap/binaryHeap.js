25d754c22616f2bc8a6143663ac7bd235b4e143f
Brian Price <brianprice88@gmail.com>
2020-05-28 13:37:31 -0400

    "refactor heap to class"

--- Heap/binaryHeap.js
+++ Heap/binaryHeap.js
@@ -5,57 +5,65 @@
 // best case O(1) since the insertion and removal themselves are constant time operations
 // however we could have to traverse the entire heap in order to correctly place the added/swapped nodes where they belong
 
-var getParentIndex = function(childIndex) {
-    return Math.floor((childIndex - 1) / 2)
-}
 
-var getChildrenIndexes = function(parentIndex) {
-    return [parentIndex * 2 + 1, parentIndex * 2 + 2]
-}
 
-var getLesserChildIndex = function(firstChild, secondChild, heap) {
-    return heap[firstChild] < heap[secondChild] ? firstChild : secondChild
-}
-
-var swapNodes = function(parentIndex, childIndex, heap) {
-    var childValue = heap[childIndex]
-    var parentValue = heap[parentIndex]
-    heap[childIndex] = parentValue
-    heap[parentValue] = childValue
-}
-
-var binaryMinHeap = function() { // values stored in an array, allowing the above parent/children functions 
-    this._storage = [];
-    this._compare = function(i, j) {return this._storage[i] < this._storage[j]} // simple comparison for node values
-}
-
-binaryMinHeap.prototype.getRoot = function() {
-    return this._storage[0]
-}
-
-binaryMinHeap.prototype.insert = function(value) {
-    this._storage.push(value); // add the new value to end of storage array and calculate its parent's index
-    var childIndex = this._storage.length - 1
-    var parentIndex = getParentIndex(childIndex)
-    while (childIndex > 0 && this._compare(childIndex, parentIndex)) { // childIndex above 0 means the child has a parent, and the compare function checks if the child's value is less than its parent
-        swapNodes(parentIndex, childIndex, this._storage) // while these conditions are true, swap child with its parent and calculate the new parent index for child's updated position
-        childIndex = parentIndex;
-        parentIndex = getParentIndex(childIndex)
+class BinaryHeap {
+    constructor() {
+        this.storage = [];
     }
-}
 
-binaryMinHeap.prototype.removeRoot = function() {
-    var lastIndex = this._storage.length - 1;
-    swapNodes(0, lastIndex, this._storage); // swap the root node with last node
-    var root = this._storage.pop(); // remove last node and return it once we're finished placing the new root in its correct position
-    var parentIndex = 0;
-    var childrenIndexes = getChildrenIndexes(parentIndex);
-    var lesserChildIndex = getLesserChildIndex (childrenIndexes[0], childrenIndexes[1], this._storage)
-    while (lesserChildIndex && this._compare(lesserChildIndex, parentIndex)) { // while the current parent has children and is greater than its smaller child, swap the two
-        swapNodes(parentIndex, lesserChildIndex, this._storage);
-        parentIndex = lesserChildIndex;
-        childrenIndexes = getChildrenIndexes(parentIndex)
-        lesserChildIndex = getLesserChildIndex(childrenIndexes[0], childrenIndexes[1], this._storage)
+    compare(firstIndex, secondIndex) {
+        return this.storage[firstIndex] < this.storage[secondIndex]
     }
-    return root;
+
+    getParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2)
+    }
+
+    getChildrenIndexes(parentIndex) {
+        return [parentIndex * 2 + 1, parentIndex * 2 + 2]
+    }
+
+    getLesserChildIndex(firstChild, secondChild, heap) {
+        return heap[firstChild] < heap[secondChild] ? firstChild : secondChild
+    }
+
+    swapNodes(parentIndex, childIndex, heap) {
+        var childValue = heap[childIndex]
+        var parentValue = heap[parentIndex]
+        heap[childIndex] = parentValue
+        heap[parentIndex] = childValue
+    }
+
+    getRoot() {
+        return this.storage[0]
+    }
+
+    insert(value) {
+        this.storage.push(value); // add the new value to end of storage array and calculate its parent's index
+        var childIndex = this.storage.length - 1
+        var parentIndex = this.getParentIndex(childIndex)
+        while (childIndex > 0 && this.compare(childIndex, parentIndex)) { // childIndex above 0 means the child has a parent, and the compare function checks if the child's value is less than its parent
+            this.swapNodes(parentIndex, childIndex, this.storage) // while these conditions are true, swap child with its parent and calculate the new parent index for child's updated position
+            childIndex = parentIndex;
+            parentIndex = this.getParentIndex(childIndex)
+        }
+    }
+
+    removeRoot() {
+        var lastIndex = this.storage.length - 1;
+        this.swapNodes(0, lastIndex, this.storage); // swap the root node with last node
+        var root = this.storage.pop(); // remove last node and return it once we're finished placing the new root in its correct position
+        var parentIndex = 0;
+        var childrenIndexes = this.getChildrenIndexes(parentIndex);
+        var lesserChildIndex = this.getLesserChildIndex(childrenIndexes[0], childrenIndexes[1], this.storage)
+        while (lesserChildIndex && this.compare(lesserChildIndex, parentIndex)) { // while the current parent has children and is greater than its smaller child, swap the two
+            this.swapNodes(parentIndex, lesserChildIndex, this.storage);
+            parentIndex = lesserChildIndex;
+            childrenIndexes = this.getChildrenIndexes(parentIndex)
+            lesserChildIndex = this.getLesserChildIndex(childrenIndexes[0], childrenIndexes[1], this.storage)
+        }
+        return root;
+    }
+
 }
